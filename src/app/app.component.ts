d6e23a54d0d39c72a7c9f5836eef20747c3bb546
Anartz Mugika <mugan86@gmail.com>
2017-03-07 08:31:46 +0100

    Change title of principal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Example to Use Angular 2 and different routes';
 }

02a63c03984c8127264d0cceac555e9a4c42a608
Anartz Mugika <mugan86@gmail.com>
2017-03-08 10:45:37 +0100

    Add new routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,7 +7,9 @@ import { DetailComponent } from './components/detail/detail.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { BootstrapUiComponent } from './components/bootstrap-ui/bootstrap-ui.component';
-
+import { BootstrapUiAccordionComponent } from './components/bootstrap-ui-accordion/bootstrap-ui-accordion.component';
+import { BootstrapUiModalComponent } from './components/bootstrap-ui-modal/bootstrap-ui-modal.component';
+import { BootstrapUiTooltipComponent } from './components/bootstrap-ui-tooltip/bootstrap-ui-tooltip.component';
 
 const appRoutes: Routes = [
         // take '/' route and send to 'home'
@@ -45,7 +47,17 @@ const appRoutes: Routes = [
     // route to bootstrap ui examples component
     {
         path: 'bootstrap-ui.accordion',
-        component:  HomeComponent
+        component:  BootstrapUiAccordionComponent
+    },
+    // route to bootstrap ui examples component
+    {
+        path: 'bootstrap-ui.modal',
+        component:  BootstrapUiModalComponent
+    },
+    // route to bootstrap ui examples component
+    {
+        path: 'bootstrap-ui.tooltip',
+        component:  BootstrapUiAccordionComponent
     },
 ];
 

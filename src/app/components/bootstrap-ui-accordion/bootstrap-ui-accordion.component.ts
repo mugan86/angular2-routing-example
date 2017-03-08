cab90861989be0b9f8c77add5bc00ed96d5b012b
Anartz Mugika <mugan86@gmail.com>
2017-03-08 20:09:02 +0100

    Accordion onlye open one item at time

--- src/app/components/bootstrap-ui-accordion/bootstrap-ui-accordion.component.ts
+++ src/app/components/bootstrap-ui-accordion/bootstrap-ui-accordion.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bootstrap-ui-accordion.component.css']
 })
 export class BootstrapUiAccordionComponent implements OnInit {
-
+  //Accordion, only one open in this time. False: open one or more in this time
+  oneAtATime = true;
   uiElementsHidden = [false, true, true];
   constructor() { }
 
